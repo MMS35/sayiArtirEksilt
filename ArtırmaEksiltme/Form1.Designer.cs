030bc4b3f6ce3c603134c4901692db38b1ad1415
Murat Mert Saral <muratmertsaral@gmail.com>
2023-06-06 16:37:52 +0300

    Ornek
    ArtirmaEksiltmeOrnek

---
+++ ArtırmaEksiltme/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace ArtırmaEksiltme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArttır = new System.Windows.Forms.Button();
+            this.btnEksilt = new System.Windows.Forms.Button();
+            this.btnSonuc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnArttır
+            // 
+            this.btnArttır.Location = new System.Drawing.Point(114, 137);
+            this.btnArttır.Name = "btnArttır";
+            this.btnArttır.Size = new System.Drawing.Size(165, 27);
+            this.btnArttır.TabIndex = 0;
+            this.btnArttır.Text = "Arttır";
+            this.btnArttır.UseVisualStyleBackColor = true;
+            this.btnArttır.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // btnEksilt
+            // 
+            this.btnEksilt.Location = new System.Drawing.Point(114, 205);
+            this.btnEksilt.Name = "btnEksilt";
+            this.btnEksilt.Size = new System.Drawing.Size(165, 32);
+            this.btnEksilt.TabIndex = 1;
+            this.btnEksilt.Text = "Eksilt";
+            this.btnEksilt.UseVisualStyleBackColor = true;
+            this.btnEksilt.Click += new System.EventHandler(this.btnEksilt_Click);
+            // 
+            // btnSonuc
+            // 
+            this.btnSonuc.Location = new System.Drawing.Point(114, 288);
+            this.btnSonuc.Name = "btnSonuc";
+            this.btnSonuc.Size = new System.Drawing.Size(165, 33);
+            this.btnSonuc.TabIndex = 2;
+            this.btnSonuc.Text = "Sonuc";
+            this.btnSonuc.UseVisualStyleBackColor = true;
+            this.btnSonuc.Click += new System.EventHandler(this.btnSonuc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSonuc);
+            this.Controls.Add(this.btnEksilt);
+            this.Controls.Add(this.btnArttır);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnArttır;
+        private System.Windows.Forms.Button btnEksilt;
+        private System.Windows.Forms.Button btnSonuc;
+    }
+}
+
---
+++ ArtırmaEksiltme/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArtırmaEksiltme
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        #region Global ve local alanlar
+        /*
+         
+                Global ve Local Alanlar
+
+        Bizim kod yazdığımız sayfada eventlerimizin kendi yaşam alanlarına local alan denir.Global alanda oluşturduğunuz değişkenler direkt olarak Ram'e çıkartılırlar..
+        Local alan kendi içerisinde oluşturulan yapıları asla dışarıya göstermez..Çünkü buradaki yapılar orada yaşar ve o yaşam alanı sonlandığında görevleri biter....
+
+        Global alan ise bu local alan dediğimiz yapıların bir kademe dışında kalan alandır..Global alanda sadece tanımlamalar ve atamalar yapılabilir...
+
+        Global alanda tanımlanmış bir değişken aynı isimde bir local alanda da tanımlanırsa local alan her zaman kendi değişkenine öncelik verir.
+        Global alanda oluşturulan bir değişken o sayfadaki local alanların hepsi tarafından görülebilir.
+
+
+        Local alandaki değişkenler local alanın işi bittiği zaman Garbage Collector tarafından Ram'den kaldırılır..Ancak Global alanda tanımlanmış olan değişkenler ramden kaldırılmazlar..
+
+         */
+
+        #endregion
+
+
+        int a = 1000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       // int a = 4;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //int a = 3;
+
+            //Eğeer bir yaşam alanı global alanda oluşturulmuş bir değişken ile aynı isme sahip bir değişkene sahipse ve siz buna rağmen global alandaki değişkene ulaşmak istiyorsanz this keyword'unu kullanmalısınız..
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            //int b = 2;
+            //b++;
+            //MessageBox.Show(b.ToString());
+            a++;
+            MessageBox.Show(a.ToString());
+
+        }
+        
+
+        private void btnEksilt_Click(object sender, EventArgs e)
+        {
+            a--;
+            MessageBox.Show(a.ToString());
+
+        }
+
+        private void btnSonuc_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(a.ToString());
+        }
+    }
+}
